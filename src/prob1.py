3e94b0c5fcbcb4d87bb5dbb154997ae6a56209e8
yumanacamura <yuma.nacamura@gmail.com>
2021-04-10 12:02:40 +0900

    設問1 監視ログファイルを指定できるようにする

--- src/prob1.py
+++ src/prob1.py
@@ -1,4 +1,8 @@
+import sys
 from datetime import datetime
+
+sys.path.append('src')
+
 from util.to_datetime import to_datetime
 
 # サーバ1つの状態を格納するクラス
@@ -13,11 +17,16 @@ class ServerState:
       # 動作状態にあるサーバからpingが返ってこない場合
       # サーバが故障したと判断して状態を更新する
       self.clash(ping_datetime)
+      return None
 
     if ping_response != '-' and self.is_working == False:
       # 故障状態にあるサーバからpingが返ってきた場合
       # サーバが復旧したと判断して状態を更新する
-      self.recover(ping_datetime)
+      # サーバの故障期間を出力する
+      return self.recover(ping_datetime)
+
+    # サーバの状態に変化がない
+    return None
 
   def clash(self, ping_datetime):
     # サーバが故障した時、動作フラグをFalseにして故障開始時刻を保存する
@@ -27,19 +36,22 @@ class ServerState:
   def recover(self, ping_datetime):
     # サーバが復旧した時、動作フラグをTrueにして故障期間を出力する
     self.is_working = True
-    print('server {address} was clashed : from {clash_time} to {recover_time}'.format(
+    return 'server {address} was clashed : from {clash_time} to {recover_time}'.format(
       address=self.server_address,
       clash_time=self.clash_from,
       recover_time=ping_datetime
-    ))
+    )
   
-  def __del__(self):
+  def finish(self):
     # サーバの監視が終了する時、サーバが故障状態ならそれを出力する
     if self.is_working == False:
-      print('server {address} was clashed : from {clash_time} to now'.format(
+      return 'server {address} was clashed : from {clash_time} to now'.format(
         address=self.server_address,
         clash_time=self.clash_from
-      ))
+      )
+
+    # サーバが動作状態なら何も出力しない
+    return None
 
 # サーバの監視ログリストからサーバの故障を検知する
 def serveillance(log_list):
@@ -47,6 +59,7 @@ def serveillance(log_list):
   server_state_dict = dict()
 
   # 監視ログを読み込んで故障を検知する
+  serveillance_result = []
   for log_line in log_list:
     # ログの文字列を各情報に分割する
     ping_datetime, server_address, ping_response = log_line.split(',')
@@ -59,5 +72,25 @@ def serveillance(log_list):
       server_state_dict[server_address] = ServerState(server_address)
     
     # サーバの状態を更新する
-    server_state_dict[server_address].update(ping_datetime, ping_response)
+    result = server_state_dict[server_address].update(ping_datetime, ping_response)
 
+    # サーバの状態について出力がある場合、保存する
+    if result != None:
+      serveillance_result.append(result)
+
+  # 各サーバの監視を終了する
+  for server in server_state_dict.values():
+    result = server.finish()
+    # サーバの状態について出力がある場合、保存する
+    if result != None:
+      serveillance_result.append(result)
+  
+  return serveillance_result
+
+if __name__ == '__main__':
+  log_file_path = input('監視ログファイル : ')
+
+  with open(log_file_path, 'r') as f:
+    log_list = [line.rstrip() for line in f.readlines()]
+
+  print('\n'.join(serveillance(log_list)))
